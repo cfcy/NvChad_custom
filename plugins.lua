356fb3ace463f4fdc02cafb84897672e7ad16132
cfcy <felix.zhu1988@gmail.com>
2023-08-27 14:19:25 +0800

    customize symbols-outline icons.

--- plugins.lua
+++ plugins.lua
@@ -95,6 +95,40 @@ local plugins = {
     init = function()
       require("core.utils").load_mappings "symbols_outline"
     end,
+    opts = {
+      -- to customize the icons, visit https://www.nerdfonts.com/cheat-sheet
+      -- and search a icon, then hover -> COPY -> Icon.
+      symbols = {
+        File = { icon = "", hl = "@text.uri" },
+        Module = { icon = "󰕳", hl = "@namespace" },
+        Namespace = { icon = "", hl = "@namespace" },
+        Package = { icon = "", hl = "@namespace" },
+        Class = { icon = "", hl = "@type" },
+        Method = { icon = "", hl = "@method" },
+        Property = { icon = "", hl = "@method" },
+        Field = { icon = "", hl = "@field" },
+        Constructor = { icon = "", hl = "@constructor" },
+        Enum = { icon = "", hl = "@type" },
+        Interface = { icon = "", hl = "@type" },
+        Function = { icon = "󰊕", hl = "@function" },
+        Variable = { icon = "", hl = "@constant" },
+        Constant = { icon = "", hl = "@constant" },
+        String = { icon = "󰅳", hl = "@string" },
+        Number = { icon = "", hl = "@number" },
+        Boolean = { icon = "", hl = "@boolean" },
+        Array = { icon = "󰅨", hl = "@constant" },
+        Object = { icon = "⦿", hl = "@type" },
+        Key = { icon = "󰌆", hl = "@type" },
+        Null = { icon = "󰟢", hl = "@type" },
+        EnumMember = { icon = "", hl = "@field" },
+        Struct = { icon = "", hl = "@type" },
+        Event = { icon = "", hl = "@type" },
+        Operator = { icon = "", hl = "@operator" },
+        TypeParameter = { icon = "", hl = "@parameter" },
+        Component = { icon = "󰡀", hl = "@function" },
+        Fragment = { icon = "󰞤", hl = "@constant" },
+      },
+    },
     config = function(_, opts)
       require("symbols-outline").setup(opts)
     end,
